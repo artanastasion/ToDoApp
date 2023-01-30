1dce2e769ebf9b916a2ed89f4c057b662a17b8da
artanastasion <103057420+artanastasion@users.noreply.github.com>
2023-01-31 08:08:30 +1000

    Repository for Task updating

--- src/main/java/com/example/myapplication/db/repository/TaskRepository.kt
+++ src/main/java/com/example/myapplication/db/repository/TaskRepository.kt
@@ -1,13 +1,14 @@
 package com.example.myapplication.db.repository
 
 import androidx.lifecycle.LiveData
+import com.example.myapplication.model.TaskListModel
 import com.example.myapplication.model.TaskModel
 
 interface TaskRepository {
 
     val allTasks : LiveData<List<TaskModel>>
     val favoriteTask : LiveData<List<TaskModel>>
-    suspend fun sortTask(int: Int)
+    fun sortTask(int: Int): LiveData<List<TaskModel>>
     suspend fun insertTask(taskModel: TaskModel, onSuccess:() -> Unit)
     suspend fun deleteTask(taskModel: TaskModel, onSuccess:() -> Unit)
     suspend fun updateTask(taskModel: TaskModel, onSuccess:() -> Unit)
